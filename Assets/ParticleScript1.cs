202c84be2fdb5e6cfe56ae641ad793f62367883e
Thomas Ruiz <lovecraft5414@gmail.com>
2019-05-26 18:10:17 -0700

    My Audio Shader Homework

---
+++ Assets/ParticleScript1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript1 : MonoBehaviour
+{
+
+    ParticleSystem ps;
+    public ParticleSystem psOne;
+
+    void Start()
+    {
+        // Get Particle system component
+        ps = GetComponent<ParticleSystem>();
+        // Call particle play function
+        ps.Play();
+    }
+
+    void Update()
+    {
+
+        int numPartitions = 8;
+        float[] aveMag = new float[numPartitions];
+        float partitionIndx = 0;
+        int numDisplayedBins = 512 / 2; //NOTE: we only display half the spectral data because the max displayable frequency is Nyquist (at half the num of bins)
+
+        for (int i = 0; i < numDisplayedBins; i++)
+        {
+            if (i < numDisplayedBins * (partitionIndx + 1) / numPartitions)
+            {
+                aveMag[(int)partitionIndx] += AudioPeer.spectrumData[i] / (512 / numPartitions);
+            }
+            else
+            {
+                partitionIndx++;
+                i--;
+            }
+        }
+
+        // scale and bound the average magnitude.
+        for (int i = 0; i < numPartitions; i++)
+        {
+            aveMag[i] = (float)0.5 + aveMag[i] * 100;
+            if (aveMag[i] > 100)
+            {
+                aveMag[i] = 100;
+            }
+        }
+
+        float mag = aveMag[0];
+
+        // if mag is greater than some threshold(0.6)
+        // emit particle using emit function
+        if (mag > 0.85)
+        {
+            ps.Emit(1);
+        }
+        if (aveMag[1] > 0.68)
+        {
+            psOne.Emit(1);
+        }
+
+    }
+
+
+}
+
---
+++ Assets/Volumecontrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volumecontrol : MonoBehaviour
+{
+    Slider slider;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = audio.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audio.volume = slider.value;
+    }
+}
